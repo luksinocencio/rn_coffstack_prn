4d2f24825ccde25b384290930874353b5a74e871
luksinocencio <luksinocencio@gmail.com>
2024-04-14 13:28:00 -0300

    01_intro_ao_react_native: ajustes no component

--- 01_intro_ao_react_native/02_fundamentos_de_react_native/FirstApp/components/Button.tsx
+++ 01_intro_ao_react_native/02_fundamentos_de_react_native/FirstApp/components/Button.tsx
@@ -1,56 +1,57 @@
-import React from 'react'
+import React from "react";
 import {
   StyleSheet,
   Text,
   TouchableOpacity,
   TouchableOpacityProps,
-} from 'react-native'
+} from "react-native";
 
 interface ButtonProps extends TouchableOpacityProps {
-  title: string
-  variant?: 'filled' | 'outlined'
+  title: string;
+  variant?: "filled" | "outlined";
 }
 
 const Button: React.FC<ButtonProps> = ({
   title,
-  variant = 'filled',
+  variant = "filled",
   style,
   ...rest
 }) => {
   const variantStyles =
-    variant === 'filled' ? styles.wrapperPrimary : styles.wrapperSecondary
-  const textStyle = variant === 'filled' ? '#fff' : '#1d013f'
+    variant === "filled" ? styles.wrapperPrimary : styles.wrapperSecondary;
+  const textStyle = variant === "filled" ? "#fff" : "#1d013f";
 
   return (
     <TouchableOpacity
       {...rest}
-      style={[styles.wrapperCommon, variantStyles, style]}>
+      style={[styles.wrapperCommon, variantStyles, style]}
+    >
       <Text style={[styles.title, { color: textStyle }]}>{title}</Text>
     </TouchableOpacity>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   wrapperCommon: {
     height: 50,
-    width: '80%',
+    width: "80%",
     borderRadius: 25,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   wrapperPrimary: {
-    backgroundColor: '#1d013f',
+    backgroundColor: "#1d013f",
   },
   wrapperSecondary: {
-    backgroundColor: 'transparent',
-    borderColor: '#1d013f',
+    backgroundColor: "transparent",
+    borderColor: "#1d013f",
     borderWidth: 2,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
-    color: '#fff',
+    fontWeight: "bold",
+    color: "#fff",
   },
-})
+});
 
-export default Button
+export default Button;
