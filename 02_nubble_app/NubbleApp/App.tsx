b28577ce621f3f64d2cf23830957fc6b1fc3065b
luksinocencio <luksinocencio@gmail.com>
2024-04-16 04:13:09 -0300

    NubbleApp(feat): criacao component Text

---
+++ 02_nubble_app/NubbleApp/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import { Text } from './src/components/Text/Text';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text preset="headingLarge" style={{ color: 'purple' }}>
+        Hello World!
+      </Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
