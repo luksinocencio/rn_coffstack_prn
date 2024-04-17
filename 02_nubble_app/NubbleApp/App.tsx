176fd3eff7cc6df88fff6375999fac08982c8aa0
luksinocencio <luksinocencio@gmail.com>
2024-04-17 14:22:37 -0300

    NubbleApp(feat): icon - criacao e configuracao componente

--- 02_nubble_app/NubbleApp/App.tsx
+++ 02_nubble_app/NubbleApp/App.tsx
@@ -2,23 +2,27 @@ import { ThemeProvider } from '@shopify/restyle';
 import React from 'react';
 import { SafeAreaView, View } from 'react-native';
 import { Button } from './src/components/Button/Button';
+import { Icon } from './src/components/Icon/Icon';
 import { Text } from './src/components/Text/Text';
 import { theme } from './src/theme/theme';
+import { Box } from './src/components/Box/Box';
 
 function App(): React.JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
         <View style={{ paddingHorizontal: 10 }}>
-          <Text preset="headingLarge" medium italic>
-            Coffstack
-          </Text>
+          <Text preset="headingLarge">Coffstack</Text>
           <Button title="Entrar" />
           <Button title="Entrar" loading marginTop="s10" />
           <Button title="Entrar" marginTop="s10" disabled />
           <Button title="Outline" preset="outline" marginTop="s10" />
           <Button title="Outline" preset="outline" marginTop="s10" disabled />
           <Button title="Outline" preset="outline" marginTop="s10" loading />
+          <Box flexDirection="row">
+            <Icon name="eyeOn" color="primary" size={20} />
+            <Icon name="eyeOff" color="primary" size={20} />
+          </Box>
         </View>
       </SafeAreaView>
     </ThemeProvider>
--- 02_nubble_app/NubbleApp/src/components/ActivityIndicator/ActivityIndicator.tsx
+++ 02_nubble_app/NubbleApp/src/components/ActivityIndicator/ActivityIndicator.tsx
@@ -3,8 +3,8 @@ import {
   ActivityIndicator as RNActivityIndicator,
   ActivityIndicatorProps as RNActivityIndicatorProps,
 } from 'react-native';
-import { Theme, ThemeColors } from '../../theme/theme';
-import { useTheme } from '@shopify/restyle';
+import { ThemeColors } from '../../theme/theme';
+import { useAppTheme } from '../../hooks/useAppTheme';
 
 interface ActivityIndicatorProps
   extends Omit<RNActivityIndicatorProps, 'color'> {
@@ -12,7 +12,7 @@ interface ActivityIndicatorProps
 }
 
 export function ActivityIndicator({ color }: ActivityIndicatorProps) {
-  const { colors } = useTheme<Theme>();
+  const { colors } = useAppTheme();
 
   return <RNActivityIndicator color={colors[color]} />;
 }
