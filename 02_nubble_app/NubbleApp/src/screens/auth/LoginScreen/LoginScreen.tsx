c45669b14426a1db67301c4dbb9920ca6098da88
luksinocencio <luksinocencio@gmail.com>
2024-04-20 03:45:31 -0300

    NubbleApp(feat): form implementar react-hook-form

--- 02_nubble_app/NubbleApp/src/screens/auth/LoginScreen/LoginScreen.tsx
+++ 02_nubble_app/NubbleApp/src/screens/auth/LoginScreen/LoginScreen.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { Alert } from 'react-native';
 import { NativeStackScreenProps } from 'react-native-screens/lib/typescript/native-stack/types';
 import { TouchableOpacityBox } from '../../../components/Box/Box';
 import { Button } from '../../../components/Button/Button';
-import { Icon } from '../../../components/Icon/Icon';
+import { PasswordInput } from '../../../components/PasswordInput/PasswordInput';
 import { Screen } from '../../../components/Screen/Screen';
 import { Text } from '../../../components/Text/Text';
 import { TextInput } from '../../../components/TextInput/TextInput';
@@ -14,13 +16,30 @@ import { RootStackParamList } from '../../../routes/Routes';
  */
 type ScreenProps = NativeStackScreenProps<RootStackParamList, 'LoginScreen'>;
 
+type LoginFormType = {
+  email: string;
+  password: string;
+};
+
 export function LoginScreen({ navigation }: ScreenProps) {
+  const { control, formState, handleSubmit } = useForm<LoginFormType>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    mode: 'onChange',
+  });
+
   function navigateToSignUpScreen() {
     navigation.navigate('SignUpScreen');
   }
 
   function navigateToForgotPasswordScreen() {
     navigation.navigate('ForgotPasswordScreen');
+  }
+
+  function submitForm({ email, password }: LoginFormType) {
+    Alert.alert('Hello', `${email} ${'\n'} ${password}`);
   }
 
   return (
@@ -32,18 +51,58 @@ export function LoginScreen({ navigation }: ScreenProps) {
         Digite seu e-mail e senha para entrar.
       </Text>
 
-      <TextInput
-        label="E-mail"
-        placeholder="Digite seu e-mail"
-        boxProps={{ mb: 's20' }}
+      <Controller
+        control={control}
+        name="email"
+        rules={{
+          required: {
+            value: true,
+            message: 'E-mail é obrigatório.',
+          },
+          pattern: {
+            value:
+              /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            message: 'E-mail inválido.',
+          },
+        }}
+        render={({ field, fieldState }) => (
+          <TextInput
+            label="E-mail"
+            errorMessage={fieldState.error?.message}
+            placeholder="Digite seu e-mail"
+            boxProps={{ mb: 's20' }}
+            value={field.value}
+            onChangeText={field.onChange}
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+        )}
       />
 
-      <TextInput
-        label="Senha"
-        placeholder="Digite a sua senha"
-        secureTextEntry
-        RightComponent={<Icon name="eyeOn" color="gray2" />}
-        boxProps={{ mb: 's20' }}
+      <Controller
+        control={control}
+        name="password"
+        rules={{
+          required: {
+            value: true,
+            message: 'Senha é obrigatória.',
+          },
+          minLength: {
+            value: 8,
+            message: 'A senha deve conter no mínimo 8 caracteres.',
+          },
+        }}
+        render={({ field, fieldState }) => (
+          <PasswordInput
+            label="Senha"
+            errorMessage={fieldState.error?.message}
+            placeholder="Digite a sua senha"
+            boxProps={{ mb: 's20' }}
+            value={field.value}
+            onChangeText={field.onChange}
+          />
+        )}
       />
 
       <TouchableOpacityBox onPress={navigateToForgotPasswordScreen}>
@@ -52,7 +111,13 @@ export function LoginScreen({ navigation }: ScreenProps) {
         </Text>
       </TouchableOpacityBox>
 
-      <Button title="Entrar" mt="s48" />
+      <Button
+        title="Entrar"
+        mt="s48"
+        onPress={handleSubmit(submitForm)}
+        disabled={!formState.isValid}
+      />
+
       <Button
         title="Criar Conta"
         preset="outline"
