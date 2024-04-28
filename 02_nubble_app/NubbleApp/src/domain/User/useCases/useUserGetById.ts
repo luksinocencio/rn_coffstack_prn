1287582b440a68ed0bdbccd6ae4cff1c69844967
luksinocencio <luksinocencio@gmail.com>
2024-04-28 14:53:22 -0300

    NubbleApp(feat): user domain - configuracao e stale time

--- 02_nubble_app/NubbleApp/src/domain/User/useCases/useUserGetById.ts
+++ 02_nubble_app/NubbleApp/src/domain/User/useCases/useUserGetById.ts
@@ -8,6 +8,7 @@ export function useUserGetById(id: number) {
   const { data, isLoading, isError } = useQuery({
     queryKey: [QueryKeys.UserGetById, id],
     queryFn: () => userService.getById(id),
+    staleTime: 1000 * 10, // 10 seconds
   })
 
   return {
