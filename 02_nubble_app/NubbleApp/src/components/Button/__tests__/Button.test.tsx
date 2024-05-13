8548d4823ba42bf49b9d8ac1ad94f69597ba42b2
luksinocencio <luksinocencio@gmail.com>
2024-05-13 18:05:25 -0300

    NubbleApp(feat): test - interacao com elementos

--- 02_nubble_app/NubbleApp/src/components/Button/__tests__/Button.test.tsx
+++ 02_nubble_app/NubbleApp/src/components/Button/__tests__/Button.test.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
-import { render } from 'test-utils'
+import { fireEvent, render } from 'test-utils'
 
 import { Button } from '../Button'
 
 describe('<Button />', () => {
   it('should render correctly', () => {
     render(<Button title="button title" />)
+  })
+
+  it('should calls the onPress when is pressed', () => {
+    const mockedOnPress = jest.fn()
+
+    const { getByText } = render(
+      <Button title="button title" onPress={mockedOnPress} />,
+    )
+
+    fireEvent.press(getByText(/button title/i))
+
+    expect(mockedOnPress).toHaveBeenCalled()
   })
 })
