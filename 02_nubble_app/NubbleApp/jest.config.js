5999a1427048e9eb75712ca78b45b3ca8423fdee
luksinocencio <luksinocencio@gmail.com>
2024-05-13 17:42:30 -0300

    NubbleApp(feat): test - render customizado

--- 02_nubble_app/NubbleApp/jest.config.js
+++ 02_nubble_app/NubbleApp/jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
   setupFilesAfterEnv: ['@testing-library/react-native/extend-expect'],
   collectCoverageFrom: ['src/{components,utils}/**/*.{js,jsx,ts,tsx}'],
   coveragePathIgnorePatterns: ['/node_modules/', 'index'],
+  moduleDirectories: ['node_modules', './src/test'],
 }
---
+++ 02_nubble_app/NubbleApp/src/test/test-utils.tsx
@@ -0,0 +1,20 @@
+import React, { ReactElement, ReactNode } from 'react'
+
+import { ThemeProvider } from '@shopify/restyle'
+import { RenderOptions, render } from '@testing-library/react-native'
+
+import { theme } from '@theme'
+
+const AllTheProviders = ({ children }: { children: ReactNode }) => {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>
+}
+
+function customRender<T = unknown>(
+  component: ReactElement<T>,
+  options?: Omit<RenderOptions, 'wrapper'>,
+) {
+  return render(component, { wrapper: AllTheProviders, ...options })
+}
+
+export * from '@testing-library/react-native'
+export { customRender as render }
