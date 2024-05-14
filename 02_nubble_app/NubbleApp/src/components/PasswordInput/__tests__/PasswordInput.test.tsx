9d94bf6c27c87c0a30e5b7eb77ebe66bf89c6a0d
luksinocencio <luksinocencio@gmail.com>
2024-05-14 17:52:22 -0300

    NubbleApp(feat): test - testando propriedades do componente

---
+++ 02_nubble_app/NubbleApp/src/components/PasswordInput/__tests__/PasswordInput.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { render, screen } from 'test-utils'
+
+import { PasswordInput } from '../PasswordInput'
+
+describe('<PasswordInput />', () => {
+  it('should start with hidden password', () => {
+    const mockedOnChange = jest.fn()
+
+    render(
+      <PasswordInput
+        label="Password"
+        placeholder="password"
+        value="123456"
+        onChangeText={mockedOnChange}
+      />,
+    )
+
+    const inputElement = screen.getByPlaceholderText('password')
+
+    expect(inputElement.props.secureTextEntry).toBeTruthy()
+  })
+})
