e9724f3735b7e2334129435123535c85b744d733
luksinocencio <luksinocencio@gmail.com>
2024-05-15 16:59:35 -0300

    NubbleApp(feat): test - test react query setup

--- 02_nubble_app/NubbleApp/jest.config.js
+++ 02_nubble_app/NubbleApp/jest.config.js
@@ -2,7 +2,9 @@ module.exports = {
   preset: 'react-native',
   setupFiles: ['<rootDir>/src/test/jestSetup.ts'],
   setupFilesAfterEnv: ['@testing-library/react-native/extend-expect'],
-  collectCoverageFrom: ['src/{components,utils}/**/*.{js,jsx,ts,tsx}'],
+  collectCoverageFrom: [
+    'src/{components,utils,hooks,domain}/**/*.{js,jsx,ts,tsx}',
+  ],
   coveragePathIgnorePatterns: ['/node_modules/', 'index'],
   moduleDirectories: ['node_modules', './src/test'],
   modulePathIgnorePatterns: ['.*/mockedData/.*'],
