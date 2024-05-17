2eb1341c0e7ed9edb3912ddba28aba3820bb63cf
luksinocencio <luksinocencio@gmail.com>
2024-05-16 22:00:47 -0300

    NubbleApp(feat): test - promise reject

--- 02_nubble_app/NubbleApp/src/domain/Auth/useCases/useAuthSignIn.ts
+++ 02_nubble_app/NubbleApp/src/domain/Auth/useCases/useAuthSignIn.ts
@@ -22,6 +22,9 @@ export function useAuthSignIn(options?: MutationOptions<AuthCredentials>) {
       }
     },
     onSuccess: authCredentials => {
+      if (options?.onSuccess) {
+        options.onSuccess(authCredentials)
+      }
       saveCredentials(authCredentials)
     },
   })
@@ -29,5 +32,7 @@ export function useAuthSignIn(options?: MutationOptions<AuthCredentials>) {
   return {
     isLoading: mutation.isLoading,
     signIn: (variables: Variables) => mutation.mutate(variables),
+    isSuccess: mutation.isSuccess,
+    isError: mutation.isError,
   }
 }
