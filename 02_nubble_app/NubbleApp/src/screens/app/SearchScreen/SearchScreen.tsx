e840427d66bc70c7ebb784fdb8fa056826001ae8
luksinocencio <luksinocencio@gmail.com>
2024-05-27 00:14:28 -0300

    NubbleApp(feat): pesquisa usuario - setup inicial

---
+++ 02_nubble_app/NubbleApp/src/screens/app/SearchScreen/SearchScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { Screen, Text } from '@components'
+import { AppScreenProps } from '@routes'
+
+export function SearchScreen({}: AppScreenProps<'SearchScreen'>) {
+  return (
+    <Screen>
+      <Text>Search Screen</Text>
+    </Screen>
+  )
+}
--- 02_nubble_app/NubbleApp/src/screens/index.ts
+++ 02_nubble_app/NubbleApp/src/screens/index.ts
@@ -7,6 +7,7 @@ export * from './app/MyProfileScreen/MyProfileScreen'
 export * from './app/NewPostScreen/NewPostScreen'
 export * from './app/PostCommentScreen/PostCommentScreen'
 export * from './app/ProfileScreen/ProfileScreen'
+export * from './app/SearchScreen/SearchScreen'
 export * from './app/SettingsScreen/SettingsScreen'
 /**
  * auth screens
