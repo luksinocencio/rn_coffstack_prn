5169eadb1871a5d48f3fd4027028b59746f44911
luksinocencio <luksinocencio@gmail.com>
2024-05-29 17:20:08 -0300

    NubbleApp(feat): pesquisa usuario - caso de uso com paginacao busca de usuario

--- 02_nubble_app/NubbleApp/src/domain/User/index.ts
+++ 02_nubble_app/NubbleApp/src/domain/User/index.ts
@@ -1,4 +1,8 @@
+// useCases
 export * from './useCases/useUserGetById'
+export * from './useCases/useUserSearch'
+
+// user
 export * from './userAdapter'
 export * from './userService'
 export * from './userTypes'
--- 02_nubble_app/NubbleApp/src/screens/app/SearchScreen/SearchScreen.tsx
+++ 02_nubble_app/NubbleApp/src/screens/app/SearchScreen/SearchScreen.tsx
@@ -1,21 +1,31 @@
 import React, { useState } from 'react'
 
 import { Icon, Screen, Text, TextInput } from '@components'
+import { useUserSearch } from '@domain'
+import { useDebounce } from '@hooks'
 import { AppScreenProps } from '@routes'
 
 export function SearchScreen({}: AppScreenProps<'SearchScreen'>) {
   const [search, setSearch] = useState('')
+  const debouncedSearch = useDebounce(search, 500)
+
+  const { list } = useUserSearch(debouncedSearch)
+
   return (
     <Screen
       canGoBack
       HeaderComponent={
         <TextInput
+          placeholder="Digite para buscar"
           value={search}
           onChangeText={setSearch}
           LeftComponent={<Icon name="search" color="gray3" />}
         />
       }>
       <Text preset="headingMedium">Buscar recentes</Text>
+      {list.map(user => (
+        <Text key={user.id}>{user.username}</Text>
+      ))}
     </Screen>
   )
 }
