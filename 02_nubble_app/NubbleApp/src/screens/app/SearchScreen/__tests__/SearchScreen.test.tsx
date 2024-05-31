330a61c03f6fdc530774b9f2499bc1e552ee3602
luksinocencio <luksinocencio@gmail.com>
2024-05-31 02:07:12 -0300

    NubbleApp(test): pesquisa usuario - setup

---
+++ 02_nubble_app/NubbleApp/src/screens/app/SearchScreen/__tests__/SearchScreen.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { renderScreen } from 'test-utils'
+
+import { SearchScreen } from '../SearchScreen'
+
+describe('integration: SearchScreen', () => {
+  renderScreen(<SearchScreen />)
+})
