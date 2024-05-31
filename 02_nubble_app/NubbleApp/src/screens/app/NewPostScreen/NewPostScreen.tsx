b3aaaafaa2081c7f5ffd8cc12e9d0757cdb9d9da
luksinocencio <luksinocencio@gmail.com>
2024-05-31 18:46:00 -0300

    NubbleApp(refactor): new post - acessando fotos

--- 02_nubble_app/NubbleApp/src/screens/app/NewPostScreen/NewPostScreen.tsx
+++ 02_nubble_app/NubbleApp/src/screens/app/NewPostScreen/NewPostScreen.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import { Image } from 'react-native'
 
 import { Screen, Text } from '@components'
 import { AppTabScreenProps } from '@routes'
+import { useCameraRoll } from '@services'
 
 export function NewPostScreen({}: AppTabScreenProps<'NewPostScreen'>) {
+  const { list } = useCameraRoll()
   return (
-    <Screen>
+    <Screen scrollable>
       <Text preset="headingLarge">New Post Screen</Text>
+      {list.map(photo => (
+        <Image
+          source={{ uri: photo }}
+          key={photo}
+          style={{ width: 200, height: 200 }}
+        />
+      ))}
     </Screen>
   )
 }
