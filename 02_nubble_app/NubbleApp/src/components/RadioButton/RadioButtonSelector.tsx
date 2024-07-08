2dc3c4fb3a40436e0a71c63c10d7cfbd39e6ae0b
luksinocencio <luksinocencio@gmail.com>
2024-07-08 01:39:32 -0300

    NubbleApp(feat): typescript generics em components

--- 02_nubble_app/NubbleApp/src/components/RadioButton/RadioButtonSelector.tsx
+++ 02_nubble_app/NubbleApp/src/components/RadioButton/RadioButtonSelector.tsx
@@ -3,18 +3,38 @@ import React from 'react'
 import { Box } from '../Box/Box'
 import { Separator } from '../Separator/Separator'
 
-import { RadioButtonItem, RadioButtonItemProps } from './RadioButtonItem'
+import { RadioButtonItem } from './RadioButtonItem'
 
-export function RadioButtonSelector({
+type ItemTConstraint = Record<string, any>
+export type RadioButtonSelectorProps<ItemT extends ItemTConstraint> = {
+  items: ItemT[]
+  selectedItem?: ItemT
+  onSelect: (item: ItemT) => void
+  labelKey: keyof ItemT
+  descriptionKey: keyof ItemT
+  valueKey: keyof ItemT
+}
+
+export function RadioButtonSelector<ItemT extends ItemTConstraint>({
   items,
-}: {
-  items: RadioButtonItemProps[]
-}) {
+  selectedItem,
+  onSelect,
+  labelKey,
+  descriptionKey,
+  valueKey,
+}: RadioButtonSelectorProps<ItemT>) {
   return (
     <Box>
       {items.map((item, index) => (
         <Box key={item.label}>
-          <RadioButtonItem {...item} />
+          <RadioButtonItem
+            label={item[labelKey]}
+            description={item[descriptionKey]}
+            onPress={() => onSelect(item)}
+            isSelected={
+              !!selectedItem && selectedItem[valueKey] === item[valueKey]
+            }
+          />
           {index < items.length - 1 && <Separator />}
         </Box>
       ))}
