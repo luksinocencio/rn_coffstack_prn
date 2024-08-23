3be53fb8441b7bceb5cb50661eddbcd7f88e258e
Lucas Inocêncio <luksinocencio@gmail.com>
2024-08-23 09:28:07 -0300

    NubbleApp(feat): atualizacao para o android

--- 02_nubble_app/NubbleApp/src/services/permission/permissionService.android.ts
+++ 02_nubble_app/NubbleApp/src/services/permission/permissionService.android.ts
@@ -29,10 +29,12 @@ async function request(name: PermissionName): Promise<PermissionStatus> {
 }
 
 function mapNameToPermission(name: PermissionName): Permission | null {
+  const platform = Platform.Version as number
+
   switch (name) {
     case 'photoLibrary':
       // @ts-ignore
-      if (Platform.Version >= 33) {
+      if (platform >= 33) {
         return 'android.permission.READ_MEDIA_IMAGES'
       } else {
         return 'android.permission.READ_EXTERNAL_STORAGE'
