7245a37f9a558cae28846bcf88a546e2baed6fee
luksinocencio <luksinocencio@gmail.com>
2025-04-08 21:42:34 -0300

    feat(NubbleApp): android update

--- 02_nubble_app/NubbleApp/android/app/src/main/java/com/nubbleapp/MainApplication.kt
+++ 02_nubble_app/NubbleApp/android/app/src/main/java/com/nubbleapp/MainApplication.kt
@@ -1,7 +1,4 @@
-package com.nubbleapp
-import android.content.res.Configuration
-import expo.modules.ApplicationLifecycleDispatcher
-import expo.modules.ReactNativeHostWrapper
+package com.nubbleapp;
 
 import android.app.Application
 import com.facebook.react.PackageList
@@ -12,13 +9,13 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.soloader.OpenSourceMergedSoMapping
+import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
-    ReactNativeHostWrapper(this, object : DefaultReactNativeHost(this) {
+    object : DefaultReactNativeHost(this) {
       override fun getPackages(): List<ReactPackage> =
         PackageList(this).packages.apply {
           // Packages that cannot be autolinked yet can be added manually here, for example:
@@ -31,23 +28,18 @@ class MainApplication : Application(), ReactApplication {
 
       override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
       override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-    })
+    }
 
   override val reactHost: ReactHost
-    get() = ReactNativeHostWrapper.createReactHost(this.applicationContext, reactNativeHost)
+    get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, OpenSourceMergedSoMapping)
+    SoLoader.init(this, false)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
     }
-    ApplicationLifecycleDispatcher.onApplicationCreate(this)
-  }
-
-  override fun onConfigurationChanged(newConfig: Configuration) {
-    super.onConfigurationChanged(newConfig)
-    ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
+    ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
   }
 }
