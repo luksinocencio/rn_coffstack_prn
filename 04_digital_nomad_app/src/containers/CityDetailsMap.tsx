38bb30bd8b7b0e5145a2c97e2c0c86d7633672d3
luksinocencio <luksinocencio@gmail.com>
2025-11-16 23:17:29 -0300

    *

--- 04_digital_nomad_app/src/containers/CityDetailsMap.tsx
+++ 04_digital_nomad_app/src/containers/CityDetailsMap.tsx
@@ -1,10 +1,35 @@
+import MapView from 'react-native-maps'
+
 import { Box } from '../components/Box'
 import { Text } from '../components/Text'
+import { useAppTheme } from '../theme/useAppTheme'
+import type { City } from '../types'
 
-export function CityDetailsMap() {
+type CityDetailsMapProps = Pick<City, 'location'>
+
+export function CityDetailsMap({ location }: CityDetailsMapProps) {
+  const { borderRadii } = useAppTheme()
   return (
     <Box padding="padding">
-      <Text>CityDetailsMap</Text>
+      <Text variant="title22" mb="s16">
+        Mapa
+      </Text>
+      <Box
+        style={{
+          width: '100%',
+          height: 200,
+          borderRadius: borderRadii.default,
+          overflow: 'hidden',
+        }}>
+        <MapView
+          style={{ width: '100%', height: '100%' }}
+          initialRegion={{
+            ...location,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </Box>
     </Box>
   )
 }
