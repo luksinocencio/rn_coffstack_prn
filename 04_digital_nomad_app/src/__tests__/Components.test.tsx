d94911dc186669808ba64c1191a1bc97981a2322
luksinocencio <luksinocencio@gmail.com>
2026-01-07 00:34:19 -0300

    *

--- 04_digital_nomad_app/src/__tests__/Components.test.tsx
+++ 04_digital_nomad_app/src/__tests__/Components.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react-native'
+import { fireEvent, render, screen, userEvent } from '@testing-library/react-native'
 import { useState } from 'react'
 import { Pressable, Text, View } from 'react-native'
 
@@ -6,7 +6,7 @@ function Component({ label, loading }: { label: string; loading: boolean }) {
   const [count, setCount] = useState(0)
 
   if (loading) {
-    return <Text>is loading...</Text>
+    return <Text>Is loading....</Text>
   }
 
   return (
@@ -20,23 +20,65 @@ function Component({ label, loading }: { label: string; loading: boolean }) {
   )
 }
 
-describe('Component: ', () => {
-  it('should display the label when is not loading', () => {
+describe('Component', () => {
+  beforeAll(() => {
+    jest.useFakeTimers()
+  })
+
+  afterAll(() => {
+    jest.useRealTimers()
+  })
+  test('should display the label when is not loading', () => {
     render(<Component label="Hello World" loading={false} />)
+
     const element = screen.getByText('Hello World')
+
     expect(element).toBeOnTheScreen()
   })
 
-  it('should display the loading text when is loading', () => {
+  it('should display the loading text when is  loading', () => {
     render(<Component label="Hello World" loading={true} />)
-    const element = screen.getByText(/is loading.../i)
-    expect(element).toBeOnTheScreen()
+
+    expect(screen.getByText(/is loading.../i)).toBeOnTheScreen()
   })
 
   it('should display the correct count number', () => {
     render(<Component label="Hello World" loading={false} />)
+
     expect(screen.getByText(/Pressed:0/i)).toBeOnTheScreen()
+
     fireEvent.press(screen.getByTestId('label-button'))
+
     expect(screen.getByText(/Pressed:1/i)).toBeOnTheScreen()
+  })
+
+  it('should display reset the count when press the reset text', async () => {
+    render(<Component label="Hello World" loading={false} />)
+
+    expect(screen.getByText(/Pressed:0/i)).toBeOnTheScreen()
+
+    const user = userEvent.setup()
+    await user.press(screen.getByText('Hello World'))
+    await user.press(screen.getByText('Hello World'))
+    await user.press(screen.getByText('Hello World'))
+    await user.press(screen.getByText('Hello World'))
+
+    expect(screen.getByText(/Pressed:4/i)).toBeOnTheScreen()
+  })
+})
+
+describe('User Event', () => {
+  it('should display reset the count when press the reset text 2', async () => {
+    render(<Component label="Hello World" loading={false} />)
+
+    expect(screen.getByText(/Pressed:0/i)).toBeOnTheScreen()
+
+    const user = userEvent.setup()
+    await user.press(screen.getByText('Hello World'))
+    await user.press(screen.getByText('Hello World'))
+    await user.press(screen.getByText('Hello World'))
+    await user.press(screen.getByText('Hello World'))
+
+    expect(screen.getByText(/Pressed:4/i)).toBeOnTheScreen()
   })
 })
